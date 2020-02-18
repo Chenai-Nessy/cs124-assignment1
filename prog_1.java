481ab0c827b38eac3d69aa6d8ad26221758b4f7f
pwmichalak <pwemichalak@gmail.com>
2020-02-18 15:46:09 -0500

    started graphGenerator function

--- prog_1.java
+++ prog_1.java
@@ -1,5 +1,8 @@
 public class prog_1 {
-   public static void main(String[] args) {
-     // our MST program
+    public static void main(String[] args) {
+        // function to generate random undirected graph
+        public static graphGenerator(int n, ) {
+            
+        }
    }
 }
