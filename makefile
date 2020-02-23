d6172ced8ee0fa36d76e9355617756263659c06f
pwmichalak <pwemichalak@gmail.com>
2020-02-22 22:31:58 -0500

    ACTUALLY added a makefile

---
+++ makefile
@@ -0,0 +1,15 @@
+JCC = javac
+JFLAGS = -g # for debugging information
+JVM = java
+
+default: randmst.class
+
+randmst.class: randmst.java
+		$(JCC) $(JFLAGS) randmst.java
+
+randmst: default
+		$(JVM) randmst
+
+# Removes all .class files, so that the next make rebuilds them
+clean:
+		$(RM) *.class
---
+++ randmst.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class randmst {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int d = in.nextInt();
+        int n = in.nextInt();
+        // figure out how to generate random graphs
+            // figure out how to create tuples
+            // figure out how to make a makefile
+
+        // print random graphs
+        System.out.println("Number of dimensions: " + d);
+        System.out.println("Number of samples: " + n);
+    }
+}
