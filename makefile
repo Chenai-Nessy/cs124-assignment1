2fe569ea51a51f3e93d78c2b9615d7f895c283be
pwmichalak <pwemichalak@gmail.com>
2020-02-22 22:58:47 -0500

    now able to get arguments from command line

--- makefile
+++ makefile
@@ -1,15 +1,12 @@
 JCC = javac
 JFLAGS = -g # for debugging information
-JVM = java
 
 default: randmst.class
 
 randmst.class: randmst.java
-		$(JCC) $(JFLAGS) randmst.java
-
-randmst: default
-		$(JVM) randmst
+	$(JCC) $(JFLAGS) randmst.java
 
 # Removes all .class files, so that the next make rebuilds them
 clean:
-		$(RM) *.class
+	$(RM) *.class
+	rm Manifest
--- randmst.java
+++ randmst.java
@@ -1,10 +1,12 @@
-import java.util.Scanner;
-
 public class randmst {
     public static void main(String[] args){
-        Scanner in = new Scanner(System.in);
-        int d = in.nextInt();
-        int n = in.nextInt();
+        if (args.length < 4) {
+            System.out.println("Usage: java randmst.java 0 numpoints numtrials dimension");
+            return;
+        }
+        int d = Integer.parseInt(args[1]);
+        int numTrials = Integer.parseInt(args[2]);
+        int n = Integer.parseInt(args[3]);
         // figure out how to generate random graphs
             // figure out how to create tuples
             // figure out how to make a makefile
